6695f8dd781afd63ef3ad7ddf675557cc73aad63
Fara-Pinokio <fara@pinokio.computer>
2025-12-01 12:45:39 -0500

    Update start.json and pinokio.js with user changes

--- pinokio.js
+++ pinokio.js
@@ -1,16 +1,62 @@
 module.exports = {
-  version: "2.0",
+  version: "3.7",
   title: "Fara-7B Computer Use Agent", 
   description: "Microsoft's 7B parameter computer use agent with Gradio interface",
   icon: "icon.svg",
-  menu: [{
-    text: "Install",
-    href: "install.json"
-  }, {
-    text: "Start", 
-    href: "start.json"
-  }, {
-    text: "Dashboard",
-    href: "start.html"
-  }]
+  menu: async (kernel, info) => {
+    let installed = info.exists("env")
+    let running = {
+      install: info.running("install.js"),
+      start: info.running("start.js")
+    }
+    if (running.install) {
+      return [{
+        default: true,
+        icon: "fa-solid fa-plug",
+        text: "Installing",
+        href: "install.js",
+      }]
+    } else if (installed) {
+      if (running.start) {
+        let local = info.local("start.js")
+        if (local && local.url) {
+          return [{
+            default: true,
+            icon: "fa-solid fa-rocket",
+            text: "Open Web UI",
+            href: local.url,
+          }, {
+            icon: 'fa-solid fa-terminal',
+            text: "Terminal",
+            href: "start.js",
+          }]
+        } else {
+          return [{
+            default: true,
+            icon: 'fa-solid fa-terminal',
+            text: "Terminal",
+            href: "start.js",
+          }]
+        }
+      } else {
+        return [{
+          default: true,
+          icon: "fa-solid fa-power-off",
+          text: "Start",
+          href: "start.js",
+        }, {
+          icon: "fa-solid fa-plug",
+          text: "Install",
+          href: "install.js",
+        }]
+      }
+    } else {
+      return [{
+        default: true,
+        icon: "fa-solid fa-plug",
+        text: "Install",
+        href: "install.js",
+      }]
+    }
+  }
 }
